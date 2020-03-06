9c2110fe96b9834807a8d960c957c896e74b9cf5
Ingrid Zippe <ingrid_zippe@brown.edu>
2020-03-06 00:19:09 -0800

    splice correct

--- routes/auth.js
+++ routes/auth.js
@@ -171,30 +171,30 @@ module.exports = function(passport) {
       // This sample code will make a request to LinkedIn's API to retrieve and print out some
       // basic profile information for the user whose access token you provide.
       // Replace with access token for the r_liteprofile permission
-      // if (accessToken != null) {
-      //   const options = {
-      //     host: 'api.linkedin.com',
-      //     path: '/v2/me',
-      //     method: 'GET',
-      //     headers: {
-      //       'Authorization': `Bearer ${accessToken}`,
-      //       'cache-control': 'no-cache',
-      //       'X-Restli-Protocol-Version': '2.0.0'
-      //     }
-      //   };
-      //   const profileRequest = https.request(options, function(res) {
-      //     let data = '';
-      //     res.on('data', (chunk) => {
-      //       data += chunk;
-      //     });
-      //     res.on('end', () => {
-      //       console.log('gets in here?')
-      //       const profileData = JSON.parse(data);
-      //       console.log(JSON.stringify(profileData, 0, 2));
-      //     });
-      //   });
-      //   profileRequest.end();
-      // }
+      if (accessToken != null) {
+        const options = {
+          host: 'api.linkedin.com',
+          path: '/v2/me',
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'cache-control': 'no-cache',
+            'X-Restli-Protocol-Version': '2.0.0'
+          }
+        };
+        const profileRequest = https.request(options, function(res) {
+          let data = '';
+          res.on('data', (chunk) => {
+            data += chunk;
+          });
+          res.on('end', () => {
+            console.log('gets in here?')
+            const profileData = JSON.parse(data);
+            console.log(JSON.stringify(profileData, 0, 2));
+          });
+        });
+        profileRequest.end();
+      }
 
     }
 
