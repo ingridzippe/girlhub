475c9b0ab33dbbcd074a1705c4f8cf60c4292c50
Ingrid Zippe <ingrid_zippe@brown.edu>
2020-03-12 15:01:30 -0700

    print array

--- routes/auth.js
+++ routes/auth.js
@@ -224,7 +224,8 @@ module.exports = function(passport) {
             console.log(JSON.stringify(profileData, 0, 2));
             console.log("email");
             console.log(profileData.elements);
-            var profileArray = profileData.elements.split(' ');
+            var profileString = JSON.stringify(profileData.elements);
+            var profileArray = profileString.split(" ");
             console.log(profileArray);
           });
         });
