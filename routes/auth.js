b4c10c2b300bc202b1300a924bd8125af94be818
Ingrid Zippe <ingrid_zippe@brown.edu>
2020-03-21 18:06:01 -0700

    wow

--- routes/auth.js
+++ routes/auth.js
@@ -232,8 +232,8 @@ module.exports = function(passport) {
             console.log("profileData.elements");
             console.log(profileData.elements);
 
-            console.log("profileData.elements.split()");
-            console.log(profileData.elements.split(""));
+            console.log("JSON.stringify(profileData.elements)");
+            console.log(JSON.stringify(profileData.elements));
             // var profileString = JSON.stringify(profileData.elements);
             // console.log(profileString);
             // var profileArray = profileString.split(`"`);
