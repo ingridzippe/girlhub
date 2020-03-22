ca861e4428bb72005665c42593fe4062d90c499e
Ingrid Zippe <ingrid_zippe@brown.edu>
2020-03-21 17:54:09 -0700

    changed

--- routes/auth.js
+++ routes/auth.js
@@ -232,11 +232,8 @@ module.exports = function(passport) {
             console.log("profileData.elements");
             console.log(profileData.elements);
 
-            console.log("profileData.elements");
-            console.log(profileData.elements.emailAddress);
-
-            console.log("profileData.elements");
-            console.log(profileData.elements["handle~"].emailAddress);
+            console.log("profileData.elements.split()");
+            console.log(profileData.elements.split(""));
             // var profileString = JSON.stringify(profileData.elements);
             // console.log(profileString);
             // var profileArray = profileString.split(`"`);
